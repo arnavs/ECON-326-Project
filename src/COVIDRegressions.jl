a9e63f97205f169ede03a3c71279c25f325226b6
Arnav Sood <5009477+arnavs@users.noreply.github.com>
2020-03-28 13:11:09 -0700

    check in initial Julia structure

---
+++ src/COVIDRegressions.jl
@@ -0,0 +1,5 @@
+module COVIDRegressions
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using COVIDRegressions
+using Test
+
+@testset "COVIDRegressions.jl" begin
+    # Write your own tests here.
+end
